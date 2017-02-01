2111d2d60967421d534fec171c58bed2367c6841
Dominick Caponi <d.caponi1@gmail.com>
2017-01-31 21:29:17 -0800

    add apps page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,11 +3,20 @@ class IndexController < ApplicationController
   end
 
   def about
+    @app = App.new "TestTest"
   end
 
   def apps
   end
 
   def photos
+  end
+
+  private;
+  class App
+    attr_accessor :name
+    def initialize name
+      @name = name
+    end
   end
 end
