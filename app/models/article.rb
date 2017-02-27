dcd90a9fae3c9afc7ed09b831b0b1c19a46a1c00
Dominick Caponi <d.caponi1@gmail.com>
2017-02-26 21:59:55 -0800

    extract next/last link template

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@ class Article < ApplicationRecord
   def get_neighbors()
     [{name: 'next', operator: ">"}, {name: 'previous', operator: "<"}].each do | op |
       define_singleton_method (op[:name]) do |position|
-        self.class.where("id #{op[:operator]} ?", self.id).send(position.to_sym)
+        self.class.where("id #{op[:operator]} ?", self.id).send(position)
       end
     end
   end
